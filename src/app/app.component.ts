a33f01e88875386a51b6ef2f1b53f24d4cc17468
kuriapaul <pauwains@gmail.com>
2019-01-31 18:55:40 +0300

    testing travis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'travis-demo';
+  title = 'travis-demo using CT and CI';
 }
